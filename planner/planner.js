a97a82d688d4bba5ee64597bfc7734cfaf932784
Sagoogoolmoongy <sagoogoolmoongy@gmail.com>
2023-02-03 10:37:09 +0900

    삭제하시겠습니까 1차

--- planner/planner.js
+++ planner/planner.js
@@ -55,15 +55,33 @@ function showNotes() {
   
 // Function to delete a note
 function deleteNote(index) {
-    let notes = localStorage.getItem("notes");
-  
-    if (notes == null) notesObj = [];
-    else notesObj = JSON.parse(notes);
-  
-    notesObj.splice(index, 1);
-  
-    localStorage.setItem("notes", 
-        JSON.stringify(notesObj));
-  
     showNotes();
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire(
+            'Deleted!',
+            'Your file has been deleted.',
+            'success'
+          )
+        }
+        if (result.isConfirmed) {
+            let notes = localStorage.getItem("notes");
+            notesObj = JSON.parse(notes);
+  
+            notesObj.splice(index, 1);
+  
+            localStorage.setItem("notes", JSON.stringify(notesObj));
+          }
+      })
 }
+
+
+
